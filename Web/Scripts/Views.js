420dd85e6ed2e04a6ef5b7834a71e9f18cb0a360
gabykotliar <gabriel.kotliar@globallogic.com>
2012-10-19 14:05:22 -0300

    js on New.cshtml moved to typeScript class

--- Web/Scripts/Views.js
+++ Web/Scripts/Views.js
@@ -2,9 +2,39 @@ var Views;
 (function (Views) {
     (function (Project) {
         var New = (function () {
-            function New() { }
+            function New(options) {
+                this.options = options;
+                this.form = $('form');
+                this.submit = this.form.find('#submit');
+                var self = this;
+                this.submit.bind('click', null, function (e) {
+                    return self.post(e);
+                });
+            }
+            New.prototype.post = function (event) {
+                var self = this;
+                if(!this.form.valid()) {
+                    return false;
+                }
+                var data = this.form.serialize();
+                $.ajax(this.options.apiPostUrl, {
+                    type: 'POST',
+                    data: data,
+                    accepts: 'JSON',
+                    context: this
+                }).success(function (result) {
+                    self.successfulPost(result);
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    alert(textStatus);
+                });
+                return false;
+            };
+            New.prototype.successfulPost = function (result) {
+                window.location.href = this.options.successRedirectUrl + result.Name;
+            };
             return New;
-        })();        
+        })();
+        Project.New = New;        
     })(Views.Project || (Views.Project = {}));
     var Project = Views.Project;
 
--- Web/Scripts/Views.ts
+++ Web/Scripts/Views.ts
@@ -1,7 +1,59 @@
-module Views.Project 
-{ 
-    class New 
-    { 
+/// <reference path="jquery.d.ts" />
 
+interface ValidatableForm extends JQuery
+{
+    valid();
+}
+
+module Views.Project 
+{
+    interface NewConfigurationOptions { apiPostUrl: string; successRedirectUrl: string; }
+     
+    export class New 
+    { 
+        form: ValidatableForm;
+        submit: JQuery;                
+
+        constructor (public options: NewConfigurationOptions) {             
+
+            this.form = <ValidatableForm>$('form');
+            this.submit = this.form.find('#submit');
+
+            var self = this;
+
+            this.submit.bind('click', null, function (e) {
+                return self.post(e);
+            });
+        }
+
+        post(event:JQueryEventObject): bool { 
+
+            var self = this;
+
+            if (!this.form.valid()) return false;
+
+            var data = this.form.serialize();
+
+            $.ajax(this.options.apiPostUrl,
+            {
+                type: 'POST',
+                data: data,                
+                accepts: 'JSON',
+                context: this
+            })
+            .success(function (result) {
+                self.successfulPost(result);
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                alert(textStatus);
+            });
+
+            return false; //cancel default event
+        }
+
+        successfulPost(result: any): void 
+        { 
+            window.location.href = this.options.successRedirectUrl + result.Name;            
+        }
     }
 }
