c250088d5fc27e3b531da0cf09050c7110a16f4e
gabykotliar <gabriel.kotliar@globallogic.com>
2012-10-25 11:33:35 -0300

    stylecop fix

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -28,6 +28,5 @@ namespace ScrumBoard.Web
 
             //GlobalConfiguration.Configuration.Services.Add(typeof(System.Web.Http.Validation.ModelValidatorProvider), new FluentValidation.WebApi.FluentValidationModelValidatorProvider());
         }
-
     }
 }
