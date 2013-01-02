11a47edc9a37b1efd598b8a411e126cb317a78d3
Gustavo Rodriguez <guselpelado@gmail.com>
2013-01-02 16:08:07 -0300

    Persistence Test

---
+++ DatabaseSchema/dbo/Tables/Sprint.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sprint] (
+    [Id]        INT      IDENTITY (1, 1) NOT NULL,
+    [StartsAt]  DATETIME NOT NULL,
+    [EndsAt]    DATETIME NOT NULL,
+    [ProjectId] INT      NOT NULL,
+    CONSTRAINT [PK__Sprint] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Sprint_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([Id])
+);
+
---
+++ DatabaseSchema/dbo/Tables/UserStory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserStory] (
+    [Id]        INT             IDENTITY (1, 1) NOT NULL,
+    [Text]      NVARCHAR (2000) NOT NULL,
+    [Effort]    FLOAT (53)      NULL,
+    [IsDone]    BIT             CONSTRAINT [DF_UserStory_IsDone] DEFAULT ((0)) NOT NULL,
+    [SprintId]  INT             NULL,
+    [ProjectId] INT             NOT NULL,
+    CONSTRAINT [PK__UserStory] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserStory_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([Id]),
+    CONSTRAINT [FK_UserStory_Sprint] FOREIGN KEY ([SprintId]) REFERENCES [dbo].[Sprint] ([Id])
+);
+
