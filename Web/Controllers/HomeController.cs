5291b7e7104a29ebd0dfc9d744b19d56f5bf37fb
gaby kotliar <gabykotliar@gmail.com>
2013-01-13 11:52:28 -0300

    home and about pages

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -6,8 +6,6 @@ namespace ScrumBoard.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
