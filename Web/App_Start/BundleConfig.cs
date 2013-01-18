0ec81d73e425b8c2a9a581cb969dcc824b7cc8f1
gaby kotliar <gabykotliar@gmail.com>
2013-01-18 15:13:40 -0300

    js file includes moved to bundles

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -7,8 +7,11 @@ namespace ScrumBoard.Web.App_Start
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.UseCdn = true;
+
+            var jqueryCdnPath = "http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.1.min.js";
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryCdnPath).Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -18,8 +21,19 @@ namespace ScrumBoard.Web.App_Start
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scrumboard").Include(
-                        "~/Scripts/URI*"));
+            bundles.Add(new ScriptBundle("~/bundles/dynamicReadonly").Include(
+                        "~/Scripts/knockout-{version}.js",
+                        "~/Scripts/URI.js",
+                        "~/Scripts/Utils.js",
+                        "~/Scripts/ViewModels.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dynamicForms").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/knockout-{version}.js",
+                        "~/Scripts/URI.js",
+                        "~/Scripts/Utils.js",
+                        "~/Scripts/ViewModels.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
